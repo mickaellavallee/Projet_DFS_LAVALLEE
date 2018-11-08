d20bc07c80bf0053c01f48fc5063d867baf36808
Mickael Lavallee <lavallee.mickael@gmail.com>
2018-11-08 15:32:41 +0100

    recuperation du json

--- app/src/main/java/com/projetdfs/lavallee/mickapp/MainActivity.java
+++ app/src/main/java/com/projetdfs/lavallee/mickapp/MainActivity.java
@@ -1,24 +1,53 @@
 package com.projetdfs.lavallee.mickapp;
 
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private Bundle savedInstqanceState;
+    private String TAG = "GETJSON";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstqanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ImageView mImageView;
+        final TextView mTxtDisplay = (TextView) findViewById(R.id.text_main);
+        String url = "http://voyage2.corellis.eu/api/v2/homev2?lat=43&lon=6";
 
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener() {
+                    @Override
+                    public void onResponse(Object o) {
+                        // Display the first 500 characters of the response string.
+                        String response = (String)o;
+                        Log.i(TAG, response.toString());
+                        mTxtDisplay.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTxtDisplay.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
     }
-
-
 }
